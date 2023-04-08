ca268056ac96e7bc8ebcb0e3c8f1a40a2c42b85b
Liden119 <117270216+Liden119@users.noreply.github.com>
2023-04-08 15:21:04 +0200

    big changessss

--- toDoList.js
+++ toDoList.js
@@ -1,4 +1,4 @@
-tasks = [];
+tasks = ["hej"];
 newTask = "hej";
 
 const taskListElement = document.getElementById("taskList");
@@ -6,19 +6,33 @@ const taskElement = document.getElementById("addTask");
 
 function addTaskToList() {}
 
+//"clean" the "old" list and load the current list
 function updateTasks() {
   taskListElement.innerHTML = "";
   for (let task of tasks) {
+    //the "task" "element / text"
     const taskElement = document.createElement("div");
     taskElement.innerText = task;
     taskElement.classList.add("task");
     taskListElement.appendChild(taskElement);
+
+    //Completed button
+    const completedButton = document.createElement("button");
+    completedButton.innerText = "Completed";
+    taskElement.appendChild(completedButton);
+    completedButton.classList.add("completed");
+
+    //remove button
+    const removeTaskButton = document.createElement("button");
+    removeTaskButton.innerText = "Remove";
+    taskElement.appendChild(removeTaskButton);
+    removeTaskButton.classList.add("remove");
   }
 }
 
 //Input in the "search/write bar"
 const inputElement = document.createElement("input");
-inputElement.placeholder = "Task (max 12 characters)";
+inputElement.placeholder = "Task-name";
 taskElement.appendChild(inputElement);
 
 //the button to click to add tasks
@@ -29,7 +43,7 @@ taskElement.appendChild(addTaskButton);
 //click function to make the text to a new task
 // and add to the tasks array
 addTaskButton.addEventListener("click", () => {
-  if (inputElement.value.length > 0 && inputElement.value.length < 13) {
+  if (inputElement.value.length > 0) {
     newTask = inputElement.value;
     tasks.push(newTask);
     console.log(tasks);
