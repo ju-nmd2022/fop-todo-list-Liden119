298ec5cc833f51faa1c6fb968c10bb92be53b7d5
Liden119 <117270216+Liden119@users.noreply.github.com>
2023-04-08 11:29:07 +0200

    js changes

--- toDoList.js
+++ toDoList.js
@@ -1,4 +1,4 @@
-tasks = ["hej", "nej"];
+tasks = [];
 newTask = "hej";
 
 const taskListElement = document.getElementById("taskList");
@@ -6,16 +6,19 @@ const taskElement = document.getElementById("addTask");
 
 function addTaskToList() {}
 
-for (let task of tasks) {
-  const taskElement = document.createElement("div");
-  taskElement.innerText = task;
-  taskElement.classList.add("task");
-  taskListElement.appendChild(taskElement);
+function updateTasks() {
+  taskListElement.innerHTML = "";
+  for (let task of tasks) {
+    const taskElement = document.createElement("div");
+    taskElement.innerText = task;
+    taskElement.classList.add("task");
+    taskListElement.appendChild(taskElement);
+  }
 }
 
 //Input in the "search/write bar"
 const inputElement = document.createElement("input");
-inputElement.placeholder = "Task-Name";
+inputElement.placeholder = "Task (max 12 characters)";
 taskElement.appendChild(inputElement);
 
 //the button to click to add tasks
@@ -26,9 +29,12 @@ taskElement.appendChild(addTaskButton);
 //click function to make the text to a new task
 // and add to the tasks array
 addTaskButton.addEventListener("click", () => {
-  if (inputElement.value.length > 0) {
+  if (inputElement.value.length > 0 && inputElement.value.length < 13) {
     newTask = inputElement.value;
     tasks.push(newTask);
     console.log(tasks);
+    updateTasks();
   }
 });
+
+updateTasks();
